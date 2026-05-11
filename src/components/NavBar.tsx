008bd6065a707249d439c063e62a1d7b07e34752
Claire/Inkyeong <claire.inkyeong@gmail.com>
2026-05-11 16:29:06 +1200

    [Complete] NavBar including style.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
+import "../css/NavBar.css";
+
 function NavBar() {
     return (
         <nav className="navbar">
             <div className="navbar-links">
-                <a href="https://github.com/claire0321" target="_blank">
+                <a href="https://github.com/claire0321" target="_blank" className="nav-link">
                     Github
                 </a>
-                <a href="https://www.linkedin.com/in/inkyeongkim/" target="_blank">
+                <a
+                    href="https://www.linkedin.com/in/inkyeongkim/"
+                    target="_blank"
+                    className="nav-link">
                     LinkedIn
                 </a>
             </div>
